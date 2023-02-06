6fcdaf4f29061936296eab062c75e87b4c71c770
Khodja Moussa <khodja.moussa.pro@gmail.com>
2023-02-06 13:42:03 +0100

    Configure routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { RouterProvider } from "react-router-dom";
+import router from "routes";
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,1 +1,10 @@
-export let a = 1
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: "hello world"
+    }
+])
+
+export default router;

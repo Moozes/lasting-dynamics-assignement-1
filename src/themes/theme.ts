cb3a70b7cfe1b2a1bd17a2d7b9d02b96257e4ee4
Khodja Moussa <khodja.moussa.pro@gmail.com>
2023-02-09 11:39:41 +0100

    Adjust provided theme

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,15 +1,7 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-    typography: {
-        fontFamily: [
-          '"Inter"',
-          'sans-serif',
-        ].join(','),
-      },
-    palette: {
-        darkBlue: {
-            main: "#04385A"
-        }
-    }
-} as any)
+  typography: {
+    fontFamily: ['"Inter"', "sans-serif"].join(","),
+  },
+});

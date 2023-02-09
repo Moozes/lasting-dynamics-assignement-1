13d24a05bb564b054b115ed0b425f57868d20558
Khodja Moussa <khodja.moussa.pro@gmail.com>
2023-02-09 14:37:49 +0100

    Simulate api delay with a function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,11 @@
 // returns formik error as a boolean
 export function formikError(inputName: string, formik: any) {
-    return Boolean(formik.touched[inputName] && formik.errors[inputName])
+  return Boolean(formik.touched[inputName] && formik.errors[inputName]);
+}
+
+// fake an API delay
+export function fakeAPI(timer: number) {
+  return new Promise<string>((res, rej) =>
+    setTimeout(() => res("resolved"), timer)
+  );
 }
